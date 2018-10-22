cd93406527a4b54f88796e855ed1533252938811
ws903 <ws903@nyu.edu>
2018-10-22 09:39:39 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+	has_many :characters
+	has_many :show, through: :characters
+
+	def full_name
+		"#{first_name} #{last_name}"
+	end
+
+	def list_roles
+		self.characters.map {|character|
+			"#{character.name} - #{show.find_by(id: character.show_id).name}"
+		}
+	end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+	has_many :characters
+	has_many :shows, through: :characters
+	belongs_to :network
+
+	def build_network(call_letters:)
+		self.network = Network.new(call_letters: call_letters)
+	end
 end
